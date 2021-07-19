00bd7bb4e40d347a9ae4f39eb9a92be2dd667a67
yeong bin <2976622@naver.com>
2021-07-19 21:40:22 +0900

    week01-yeongbin: 04

--- week-01/임영빈/03/solution.py
+++ week-01/임영빈/03/solution.py
@@ -3,6 +3,9 @@ def solution(clothes):
 	hash_map = dict()
 	for cloth in clothes:
 		hash_map[cloth[1]] = hash_map.get(cloth[1], 0) + 1
+	
 	for i in hash_map.values():
+		# +1은 각 착용하지 않은 경우의 수
 		answer = answer * (i + 1)
+	# -1은 아무것도 안입은 경우의 수
 	return answer - 1
