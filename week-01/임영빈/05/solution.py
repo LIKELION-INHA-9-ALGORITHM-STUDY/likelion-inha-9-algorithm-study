e11cfb1458600f4d0ca395c81840e5273a1f727f
yeong bin <2976622@naver.com>
2021-07-19 21:35:37 +0900

    week01-yeongbin: 04

---
+++ week-01/임영빈/05/solution.py
@@ -0,0 +1,12 @@
+def solution(genres, plays):
+#genres_count = {“classic” : 600, “pop”: 400}[{‘genre’:classic}]
+#value기준으로 정렬
+#music = [{‘idx’:0, ‘count’:400, ‘genre’:’classic’},{},{}]
+#for genre in genres_count:
+#	count = 0
+#	for arr in music.values():
+#		if arr[1] == genre:
+#			answer.append(arr[0])
+#			count+=1
+#			if count > 1
+#				break
