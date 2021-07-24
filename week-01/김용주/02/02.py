52b1e456c464a636a1bb1508852fe10fae869472
dragon0821 <54203085+dragon0821@users.noreply.github.com>
2021-07-24 12:42:21 +0900

    update 02

--- week-01/김용주/02/02.py
+++ week-01/김용주/02/02.py
@@ -6,5 +6,6 @@ def solution(phone_book):
     for number in phone_book:
         for i in range (len(number)):
             if number[0:i] in num:
+                # number[0:i] 가 num의 key에 있는지 없는지
                 return False
     return answer
