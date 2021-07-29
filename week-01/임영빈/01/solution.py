16afe6b98c09b0eecf1194bbc4f82591e627c191
yeong bin <2976622@naver.com>
2021-07-29 22:24:03 +0900

    week02-yeongbin: 04

--- week-01/임영빈/01/solution.py
+++ week-01/임영빈/01/solution.py
@@ -1,7 +1,7 @@
 def solution(participant, completion):
 	hash_map = dict()
 	for i in participant:
-		hash_map[i] = hash_map.get(i, 0) +1
+		hash_map[i] = hash_map.get(i, 0) + 1
 	for j in completion:
 		hash_map[j] -= 1
 	for answer in hash_map:
