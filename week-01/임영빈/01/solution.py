328def87c94f403b9714acbaca065cad326e84c9
Yeongbin Im <56269396+yeongbinim@users.noreply.github.com>
2021-07-29 22:46:47 +0900

    Delete solution.py

--- week-01/임영빈/01/solution.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-	hash_map = dict()
-	for i in participant:
-		hash_map[i] = hash_map.get(i, 0) + 1
-	for j in completion:
-		hash_map[j] -= 1
-	for answer in hash_map:
-		if hash_map[answer]:
-			return answer
