eb9f9fed4f5325ae14056d1b02f78e76d10adab7
yeong bin <2976622@naver.com>
2021-07-29 19:59:20 +0900

    week02-yeongbin: 04

--- week-02/임영빈/04/solution.py
+++ week-02/임영빈/04/solution.py
@@ -1,8 +1,12 @@
 def solution(prices):
+    len_price = len(prices)
     answer = list()
     for i, price in enumerate(prices):
         time = 0
-        for j in range(i + 1 ,len(prices)):
+        # 만약 prices가 [1,2,3,1,3]이었다면
+	    # 1이랑 2,3,1,3 비교 그 다음엔,
+	    # 2랑 3,1,3 비교
+        for j in range(i + 1 ,len_price):
             time += 1
             if price > prices[j]:
                 break
--- week-02/임영빈/04/solution2.py
+++ week-02/임영빈/04/solution2.py
@@ -13,8 +13,8 @@ def solution(prices):
 	answer = list()
 	prices = deque(prices)
 	# 만약 prices가 [1,2,3,1,3]이었다면
-	# find_idx_from_deque(1, [2,3,1,3])이렇게 들어가서 4반환, 그 담에는
-	# find_idx_from_deque(2, [3,1,3]) 이렇게 들어가서 2반환
+	# find_idx_from_deque(1, [2,3,1,3])이렇게 들어가서 idx=4반환, 그 담에는
+	# find_idx_from_deque(2, [3,1,3]) 이렇게 들어가서 idx=2반환
 	while(prices):
 		idx = find_idx_from_deque(prices.popleft(), prices)
 		answer.append(idx)
