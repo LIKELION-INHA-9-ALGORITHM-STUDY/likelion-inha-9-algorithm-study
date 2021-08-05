867445c580c0da53e0065c4a3d8ea1a7a19dce67
Minhyeok <sonmh79@naver.com>
2021-08-06 03:45:19 +0900

    010405

--- week-03/손민혁/01/solution.py
+++ week-03/손민혁/01/solution.py
@@ -1,23 +1,5 @@
 def solution(array, commands):
-    return [sorted(array[start-1:end])[target-1] for start, end, target in commands]
 
-numbers = [3, 30, 34, 5, 9,3666,3665,366,3662,3667,311,3221,3115,3110,3113]
-
-import heapq
-
-answer = ""
-max_len = len(str(max(numbers)))
-print(max_len)
-heap = []
-for num in numbers:
-    num = str(num)
-    original_num = num
-    while len(num) < max_len:
-        num += num[0]
-    heapq.heappush(heap,(-int(num),original_num))
-while heap:
-    h = heapq.heappop(heap)
-    print(h)
-    answer += h[1]
-print(answer)
-
+    return [
+        sorted(array[start - 1 : end])[target - 1] for start, end, target in commands
+    ]
