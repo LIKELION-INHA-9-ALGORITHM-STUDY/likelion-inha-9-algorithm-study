4ddcf81087bf110acaa3c5027463c275a08f39ed
Minhyeok <sonmh79@naver.com>
2021-08-16 20:34:30 +0900

    01

---
+++ week-04/손민혁/01/solution.py
@@ -0,0 +1,17 @@
+class student:
+    def __init__(self, real_answer, random, id):
+        share, rest = len(real_answer) // len(random), len(real_answer) % len(random)
+        self.answer = random * share + [random[i] for i in range(rest)]
+        self.score = sum(
+            [(self.answer[i] == real_answer[i]) for i in range(len(real_answer))]
+        )
+        self.id = id
+
+
+def solution(answers):
+    s1 = student(answers, [1, 2, 3, 4, 5], id=1)
+    s2 = student(answers, [2, 1, 2, 3, 2, 4, 2, 5], id=2)
+    s3 = student(answers, [3, 3, 1, 1, 2, 2, 4, 4, 5, 5], id=3)
+
+    top_score = max(s1.score, s2.score, s3.score)
+    return [student.id for student in [s1, s2, s3] if student.score == top_score]
