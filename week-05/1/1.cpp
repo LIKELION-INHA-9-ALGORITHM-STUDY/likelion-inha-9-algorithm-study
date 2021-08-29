a52ec34f6ae6ac11691e98f90a390bc4924e68d0
soojin <sobeast742@gmail.com>
2021-08-29 16:17:08 +0900

    5주차 (greedy)  과제

---
+++ week-05/1/1.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+	int answer = 0;
+	vector<int> student(n, 1); //벡터크기가 n이고 모든 원소를 1로 초기화시키기
+
+	//student vector에 학생 상태 대입하기 
+	for (int i = 0; i < reserve.size(); i++) {
+		student[reserve[i] - 1]++;
+	}
+	for (int i = 0; i < lost.size(); i++) {
+		student[lost[i] - 1]--;
+	}
+
+	//체육복 두개인 사람이 빌려주기
+	for (int i = 0; i < n; i++) {
+		if (student[i] == 0 && student[i - 1] == 2) {
+			student[i]++; student[i - 1]--;
+		}
+		if (student[i] == 0 && student[i + 1] == 2) {
+			student[i]++; student[i + 1]--;
+		}
+	}
+
+	//체육복갖고있는 학생 수 세기
+	for (int i = 0; i < student.size(); i++) {
+		if (student[i] > 0) answer++;
+	}
+
+	return answer;
+}
